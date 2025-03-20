9115d5b02c24f28f14ef1e30252a437797c44c57
unknown <jamie.g.ii@hotmail.com>
2025-03-19 21:53:13 -0500

    Added wasd movement for spaceship and background music. So much more that I need to do tomorrow, gotta lock in.

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ func main() {
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 
+	//audio initialize, need to use streaming for background music
+	rl.InitAudioDevice()
+	chillMusic := rl.LoadMusicStream("C:/_dev/Go/cs4953/WeeklyProject7/WeeklyProject7cs4953/670039__seth_makes_sounds__chill-background-music.wav")
+	rl.PlayMusicStream(chillMusic)
+	rl.SetMusicVolume(chillMusic, 0.2)
+
 	//load the spaceship image
 	spaceShipImage := rl.LoadTexture("C:/_dev/Go/cs4953/RaylibAssets/Spaceship.png")
 	player := NewSpaceship(spaceShipImage, float32(50), float32(300), float32(10), float32(100), float32(0))
@@ -19,10 +25,26 @@ func main() {
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.SkyBlue)
+		//play audio
+		rl.UpdateMusicStream(chillMusic)
 
 		//draw the planet and spaceship, probably want to come back and fix the planet
 		rl.DrawCircle(200, 100, 40, rl.Black)
 		player.DrawCreature()
+
+		//basic movement for the spaceship, need to add use of Q and R to look around with spaceship
+		if rl.IsKeyDown(rl.KeyW) {
+			player.MoveSpaceship(0, -20)
+		}
+		if rl.IsKeyDown(rl.KeyA) {
+			player.MoveSpaceship(-20, 0)
+		}
+		if rl.IsKeyDown(rl.KeyS) {
+			player.MoveSpaceship(0, 20)
+		}
+		if rl.IsKeyDown(rl.KeyD) {
+			player.MoveSpaceship(20, 0)
+		}
 
 		rl.EndDrawing()
 	}
--- spaceship.go
+++ spaceship.go
@@ -1,8 +1,10 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
-//spaceship(player) struct to draw the spaceship for the game
+// spaceship(player) struct to draw the spaceship for the game
 type Spaceship struct {
 	Texture rl.Texture2D
 	Xpos    float32
@@ -12,7 +14,7 @@ type Spaceship struct {
 	Angle   float32
 }
 
-//new spaceship using the Spaceship struct
+// new spaceship using the Spaceship struct
 func NewSpaceship(texture rl.Texture2D, xpos, ypos, speed, size, angle float32) Spaceship {
 	return Spaceship{
 		Texture: texture,
@@ -24,8 +26,29 @@ func NewSpaceship(texture rl.Texture2D, xpos, ypos, speed, size, angle float32) 
 	}
 }
 
-//draw the spaceship to the screen
+// draw the spaceship to the screen
 func (s Spaceship) DrawCreature() {
 	scale := s.Size / float32(s.Texture.Width)
 	rl.DrawTextureEx(s.Texture, rl.Vector2{X: s.Xpos, Y: s.Ypos}, s.Angle, scale, rl.White)
+}
+
+// move spaceship function
+func (s *Spaceship) MoveSpaceship(xOffset, yOffset float32) {
+	s.Xpos += xOffset
+	s.Ypos += yOffset
+
+	//top and bottom boundaries
+	if s.Ypos < 0 {
+		s.Ypos = 0
+	}
+	if s.Ypos+s.Size > float32(rl.GetScreenHeight()) {
+		s.Ypos = float32(rl.GetScreenHeight()) - s.Size
+	}
+	//left and right boundaries
+	if s.Xpos < 0 {
+		s.Xpos = 0
+	}
+	if s.Xpos+s.Size > float32(rl.GetScreenWidth()) {
+		s.Xpos = float32(rl.GetScreenWidth()) - s.Size
+	}
 }
